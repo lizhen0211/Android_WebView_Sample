729631c1ec702fbf3692697562380d55958b26e9
lizhen <lizhen0211@163.com>
2020-05-29 19:52:16 +0800

    jsbridge

--- app/src/main/java/com/example/lz/android_webview_sample/otherjsbridge/OtherJsBridgeActivity.java
+++ app/src/main/java/com/example/lz/android_webview_sample/otherjsbridge/OtherJsBridgeActivity.java
@@ -6,8 +6,10 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.lz.android_webview_sample.R;
+import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
 import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.github.lzyzsd.jsbridge.DefaultHandler;
 import com.google.gson.Gson;
 
 public class OtherJsBridgeActivity extends Activity {
@@ -20,6 +22,18 @@ public class OtherJsBridgeActivity extends Activity {
         setContentView(R.layout.activity_other_js_bridge);
         webView = (BridgeWebView) findViewById(R.id.webView);
         webView.loadUrl("http://192.168.6.49:8081/otherJsBridge");
+
+        webView.setDefaultHandler(new DefaultHandler());
+
+        webView.registerHandler("submitFromWeb", new BridgeHandler() {
+
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                Log.i("chrom-java", "handler = submitFromWeb, data from web = " + data);
+                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
+                Log.e("chrom-java","webView.registerHandler onCallBack");
+            }
+        });
     }
 
     public void callJsWithoutCallback(View view) {

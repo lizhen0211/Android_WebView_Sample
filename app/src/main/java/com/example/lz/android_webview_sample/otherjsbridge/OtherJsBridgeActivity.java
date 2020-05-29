af5c75fbb59a5e8800b9886c2eea923fbb142556
lizhen <lizhen0211@163.com>
2020-05-29 19:17:33 +0800

    jsbridge

--- app/src/main/java/com/example/lz/android_webview_sample/otherjsbridge/OtherJsBridgeActivity.java
+++ app/src/main/java/com/example/lz/android_webview_sample/otherjsbridge/OtherJsBridgeActivity.java
@@ -2,10 +2,13 @@ package com.example.lz.android_webview_sample.otherjsbridge;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.lz.android_webview_sample.R;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.google.gson.Gson;
 
 public class OtherJsBridgeActivity extends Activity {
 
@@ -19,11 +22,35 @@ public class OtherJsBridgeActivity extends Activity {
         webView.loadUrl("http://192.168.6.49:8081/otherJsBridge");
     }
 
-    public void sendHello(View view){
-
+    public void callJsWithoutCallback(View view) {
+        webView.send("hello");
     }
 
-    public void callHandler(){
+    public void callJsWithcallback(View view) {
+        webView.callHandler("functionInJs", getJson(), new CallBackFunction() {
+            @Override
+            public void onCallBack(String data) {
+                Log.e("chrom-java", "webView.callHandler onCallBack  " + data);
+            }
+        });
+    }
 
+    public String getJson() {
+        User user = new User();
+        Location location = new Location();
+        location.address = "shenyang";
+        user.location = location;
+        user.name = "zhangsan";
+        return new Gson().toJson(user);
+    }
+
+    static class Location {
+        String address;
+    }
+
+    static class User {
+        String name;
+        Location location;
+        String testStr;
     }
 }
